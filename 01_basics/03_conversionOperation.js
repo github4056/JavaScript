32152dfaf48be806e5f98fce13cf4b30b77e10dd
Hemrix <58745826+github4056@users.noreply.github.com>
2025-06-09 09:54:54 +0000

    Conversio and Operation is complete

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+// *********************** Conversions ***********************
+
 // let score = 33       // 
 // let score = "33"     // convert into 33
 // let score = "33abc"  // convert into NaN
@@ -31,5 +33,40 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// *********************** Operations ***********************
+
+// let =value = 3
+// let negValue = -value
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Tejveer"
+let str2 = " Singh"
+let str3 = str1+str2;
+// console.log(str3)
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// -------------------------
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+

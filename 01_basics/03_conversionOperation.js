1b989b6580bf23d4629b55ffd6a47be63caaa7a1
Hemrix <58745826+github4056@users.noreply.github.com>
2025-06-09 06:06:30 +0000

    Push Convertion and Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = 33       // 
+// let score = "33"     // convert into 33
+// let score = "33abc"  // convert into NaN
+// let score = null
+// let score = undefined
+// let score = true     // convert into true = 1, false = 0
+// let score = false
+// let score = "Tejveer"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// ----------------------------------
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+// let isLoggedIn = "Tejveer"
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Tejveer" => true
+
+// ----------------------------------
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
